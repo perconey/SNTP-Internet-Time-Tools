63914bca51eec4e5e1a36e25f87ffd54fa34f9cf
Perconey <perconey@gmail.com>
2017-12-18 21:54:22 +0100

    Making project file structure suitable for MVVM

--- Internet time/Internet time/MainWindow.xaml.cs
+++ Internet time/Internet time/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Internet_time
+namespace InternetTime
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
--- Internet time/Internet time/Properties/AssemblyInfo.cs
+++ Internet time/Internet time/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-[assembly: AssemblyTitle("Internet time")]
+[assembly: AssemblyTitle("InternetTime")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Internet time")]
+[assembly: AssemblyProduct("InternetTime")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
