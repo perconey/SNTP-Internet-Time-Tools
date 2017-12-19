51c8b1d010b8294cee9623caf1dc8b6591be6051
Perconey <perconey@gmail.com>
2017-12-19 19:54:48 +0100

    Adding references and using statements of Rebex Time .Net lib

--- Internet time/Internet time/ViewModel/MainWindowViewModel.cs
+++ Internet time/Internet time/ViewModel/MainWindowViewModel.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InternetTime.ViewModel
 {
-    class ViewModel : INotifyPropertyChanged
+    class MainWindowViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+
+
     }
 }
