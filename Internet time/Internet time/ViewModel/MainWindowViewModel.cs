391f5a5c36e5712f4bc73b1ad8c4fbb4c0e1822b
Perconey <perconey@gmail.com>
2017-12-20 22:20:05 +0100

    Populating ComboBox with ServerChooser values

--- Internet time/Internet time/ViewModel/MainWindowViewModel.cs
+++ Internet time/Internet time/ViewModel/MainWindowViewModel.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InternetTime.Data;
+using System.Windows;
 
 namespace InternetTime.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
+        public MainWindowViewModel()
+        {
 
+        }
+        
+        public List<String> ServerAddresses
+        {
+            get { return ServerChooser.Servers; }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Internet time/InternetTime.Data/ServerChooser.cs
+++ Internet time/InternetTime.Data/ServerChooser.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace InternetTime.Data
 {
-    static class ServerChooser
+    public static class ServerChooser
     {
+        public static List<String> Servers = new List<string>()
+        {
+            "pl.pool.ntp.org","de.pool.ntp.org","by.pool.ntp.org","fr.pool.ntp.org"
+        };
+
         private static StringBuilder _serverAddress = new StringBuilder();
 
         public static  StringBuilder ServerAdress { get => _serverAddress; set => _serverAddress = value; }
